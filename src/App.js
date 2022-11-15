bedaf98bddc5b647bfe8633c6f2c06050e16acaf
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-11-15 23:30:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: "20px 10px"}}>
+      <div style={{textAlign: "center"}}>
+        <form action="">
+          <input type="text" name="name" id="name" />
+          <br />
+          <input type="email" name="email" id="email" />
+          <br />
+          <button type="submit">Add User</button>
+        </form>
+      </div>
+      <h1 style={{textAlign: "center"}}>Users: {users.length}</h1>
+      <ol>
+      {
+        users.map(user => <li key={user.id}>{user.name}</li>)
+      }
+      </ol>
     </div>
   );
 }
