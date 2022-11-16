75c28a109aeaaf335bd56cc0a874e5672e4d5e22
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-11-16 21:28:30 +0600

    added mongo

--- src/App.js
+++ src/App.js
@@ -8,13 +8,37 @@ function App() {
     .then(res => res.json())
     .then(data => setUsers(data))
   },[])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    fetch('http://localhost:5000/users',{
+      method:'post',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      console.log(users);
+      let newUser = [...users, data];
+      setUsers(newUser);
+    })
+    .catch(err => console.log(err))
+    form.reset();
+  }
   return (
     <div style={{padding: "20px 10px"}}>
       <div style={{textAlign: "center"}}>
-        <form action="">
-          <input type="text" name="name" id="name" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name" id="name" placeholder='Name' required/>
           <br />
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" placeholder='Email' required />
           <br />
           <button type="submit">Add User</button>
         </form>
